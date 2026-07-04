b906060b7c0a40dc2c761289670bffd165b1b237
Fireking1996 <firebringer1996@yahoo.com>
2026-07-04 13:20:20 -0500

    update

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+
+const { connectDB } = require("./db/connect");
+
+const contactsRoute = require("./routes/contacts");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/contacts", contactsRoute);
+
+const port = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+async function startServer() {
+  await connectDB();
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+startServer();
